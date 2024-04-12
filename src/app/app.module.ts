7ef8fa7fe4c43ab70c44b7d158856ab31e427be4
Vini5577 <ugopessoa@gmail.com>
2024-04-12 13:37:46 -0300

    add sections folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { HeroSectionComponent } from './components/hero-section/hero-section.component';
-import { VideoSectionComponent } from './components/video-section/video-section.component';
-import { LogoSectionComponent } from './components/logo-section/logo-section.component';
+import { HeroSectionComponent } from './components/sections/hero-section/hero-section.component';
+import { VideoSectionComponent } from './components/sections/video-section/video-section.component';
+import { LogoSectionComponent } from './components/sections/logo-section/logo-section.component';
 
 @NgModule({
   declarations: [
