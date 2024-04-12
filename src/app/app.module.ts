787b1dbbdd4cb76d6f8e0e1b54794fcb723d15f7
Vini5577 <ugopessoa@gmail.com>
2024-04-12 13:02:12 -0300

    add video section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { VideoSectionComponent } from './components/video-section/video-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeroSectionComponent
+    HeroSectionComponent,
+    VideoSectionComponent
   ],
   imports: [
     BrowserModule
